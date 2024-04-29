015c6275a8d897afa7f2ef6f5f67affc229b827c
KabooZ1987 <kabooz18@gmail.com>
2024-04-29 03:21:11 +0200

    fields move scss to assets

--- components/ColorModeButton.vue
+++ components/ColorModeButton.vue
@@ -10,18 +10,14 @@ const isDark = computed({
 })
 </script>
 <template>
-    <UButton
+    <Button
       :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-      color="white"
-      variant="ghost"
       aria-label="Theme"
       @click="isDark = !isDark"
-      :ui="{
-        color:{
-          white:{
-            ghost: 'dark:text-fuchsia-700 text-fuchsia-700 bg-zinc-800 dark:bg-zinc-800 border-zinc-600'
-          }
-        }
-      }"
+      :pt="{  
+        root:'bg-zinc-800 dark:bg-zinc-800 border-zinc-600 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] w-12 h-12 rounded-md border focus:outline-none focus:outline-offset-0 focus:ring hover:border-primary-600 dark:hover:border-primary-300 focus:ring-primary-400/50 dark:focus:ring-primary-300/50 transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none',
+        icon:'dark:text-fuchsia-700 text-fuchsia-700 h-5 w-5',
+       }"
     />
 </template>
+
