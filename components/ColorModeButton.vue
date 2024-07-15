b9bbfad0680dfd8a38601b2f343b4ac2b96a26ee
Maximilian Esch <max.esch.87@gmail.com>
2024-07-16 00:29:37 +0200

    color mode icon

--- components/ColorModeButton.vue
+++ components/ColorModeButton.vue
@@ -31,15 +31,12 @@ const toggleDarkMode = () => {
 
 <template>
     <Button
-      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      :icon="isDark ? 'pi pi-sun' : 'pi pi-moon'"
       aria-label="Theme"
       @click="toggleDarkMode"
-      :pt="{  
-        root:'bg-zinc-800 dark:bg-zinc-800 border-zinc-600 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] w-12 h-12 rounded-md border focus:outline-none focus:outline-offset-0 focus:ring hover:border-primary-600 dark:hover:border-primary-300 focus:ring-primary-400/50 dark:focus:ring-primary-300/50 transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none',
-        icon:'dark:text-fuchsia-700 text-fuchsia-700 h-5 w-5',
-       }"
-    >
-    <i :class="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" />
-    </Button>
+
+    />
 </template>
 
+<style lang="scss" scoped>
+</style>
